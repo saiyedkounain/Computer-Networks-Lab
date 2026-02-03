cd68dc5fbe39445111faade57634e02aef29fa07
Saiyed Kounain <128212021+saiyedkounain@users.noreply.github.com>
2026-02-04 04:02:15 +0530

    Implement CRC encoding and error detection

---
+++ CRC.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class CRC {
+
+    static String div(String data, String key) {
+        char[] a = data.toCharArray();
+        char[] b = key.toCharArray();
+        int n = b.length;
+
+        for (int i = 0; i <= a.length - n; i++) {
+            if (a[i] == '1')
+                for (int j = 0; j < n; j++)
+                    a[i + j] = (a[i + j] == b[j]) ? '0' : '1';
+        }
+        return new String(a).substring(a.length - n + 1);
+    }
+
+    static String encode(String data, String key) {
+        return data + div(data + "0".repeat(key.length() - 1), key);
+    }
+
+    static boolean error(String code, String key) {
+        return div(code, key).contains("1");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Key: ");
+        String key = sc.next();
+
+        System.out.print("Data: ");
+        String data = sc.next();
+        String code = encode(data, key);
+
+        System.out.println("Encoded: " + code);
+
+        System.out.print("Enter received code: ");
+        System.out.println(error(sc.next(), key) ?
+                "Error detected" : "No error");
+    }
+}
